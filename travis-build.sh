8bd61d3bddf26609b9c3ba0b50f0065f4f57f61a
Anupam Basak <anupam.basak27@gmail.com>
2020-06-11 01:56:59 +0530

    Initial Commit

---
+++ travis-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get --yes update
+apt-get --yes install git wget curl qttools5-dev libyaml-cpp-dev libpolkit-qt5-1-dev
+
+git clone https://github.com/milohr/calamares.git -b qml --depth 1
+mkdir -p calamares/build
+(
+    cd calamares/build
+
+    cp ../../cpack-deb.cmake ../
+    printf "\ninclude(./cpack-deb.cmake)\n" >> ../CMakeLists.txt
+
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    make -j$(nproc) package
+)
