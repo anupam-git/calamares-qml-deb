698a8b82c87a1280e45f51733b77c1eebb91604a
Anupam Basak <anupam.basak27@gmail.com>
2020-06-23 22:26:01 +0530

    Add dependencies

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -ex
+
 apt-get --yes update
-apt-get --yes install git wget curl qttools5-dev libyaml-cpp-dev libpolkit-qt5-1-dev
+apt-get --yes install git wget curl qttools5-dev libyaml-cpp-dev libpolkit-qt5-1-dev libkf5crash-dev python3-dev libkpmcore-dev libkf5parts-dev libappstreamqt-dev libkf5plasma-dev libkf5package-dev libpwquality-dev libparted-dev libpythonqt-qt5-common-dev libpythonqt-qtall-qt5-common-dev libboost-dev libboost-python-dev
 
 git clone https://github.com/milohr/calamares.git -b qml --depth 1
 mkdir -p calamares/build
